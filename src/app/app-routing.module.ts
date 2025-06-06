a91239fb5e15857f10b952900c2e49c17c02bd5d
AbdellahElahmar <elahmar.abdellah@gmail.com>
2025-06-06 20:19:21 +0100

    feat: login process

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./features/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
